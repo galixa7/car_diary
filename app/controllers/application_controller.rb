3dbad7cd85962642c5e8e9d9f407c3b9fec98b96
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-08-10 20:14:41 +0300

    Fixing app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
-    devise_parameter_sanitizer.permit :account_update, keys: [:content, added_attrs]
+  def configure_permitted_parameters    
+    devise_parameter_sanitizer.permit :sign_up, keys: [:username, :email]    
+    devise_parameter_sanitizer.permit :account_update, keys: [:content, :username, :email]
   end
 
   protected
